effef9fbad8b797edf11d4289f66b4faf41db3bb
swoo0514 <swoo0514lim@gmail.com>
2024-07-30 15:43:20 +0900

    쿼리문 재작성

--- src/models/goal-model.js
+++ src/models/goal-model.js
@@ -1,7 +1,8 @@
 const pool = require('../../config/database');
 
 exports.getGoalsByDateRange = async (week_start, week_end) => {
-  const query = 'SELECT * FROM goals WHERE start_date >= ? AND end_date <= ?';
+  const query =
+    'select g.id, title, description, start_date, end_date, type, status, latitude, longitude,  validation_type, emoji, donation_organization_id, donation_amount, gi.date from goals g, goalinstances gi where g.id = gi.goal_id and start_date >= ? and end_date <= ?';
 
   const [rows] = await pool.execute(query, [week_start, week_end]);
   return rows;
