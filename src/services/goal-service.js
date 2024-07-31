298acb755b958d469dda8f853879909853edf0b3
swoo0514 <swoo0514lim@gmail.com>
2024-07-31 18:44:40 +0900

    응답 데이터 수정

--- src/services/goal-service.js
+++ src/services/goal-service.js
@@ -1,3 +1,4 @@
+// ./src/services/goal-service.js
 const { getGoalsByDateRange } = require('../models/goal-model');
 
 exports.getGoals = async (week_start, week_end) => {
@@ -6,7 +7,14 @@ exports.getGoals = async (week_start, week_end) => {
   const response = {
     week_start,
     week_end,
-    goals,
+    goals: goals.reduce((acc, goal) => {
+      const goalDate = goal.date.toISOString().split('T')[0];
+      if (!acc[goalDate]) {
+        acc[goalDate] = { date: goalDate, goals: [] };
+      }
+      acc[goalDate].goals.push(goal);
+      return acc;
+    }, {}),
   };
 
   response.goals = Object.values(response.goals).sort(
