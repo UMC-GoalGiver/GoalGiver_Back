7c8abaa0bd9b186769c725e2542a0f6f3150ec77
swoo0514 <swoo0514lim@gmail.com>
2024-07-31 15:27:10 +0900

    카메라 인증 기능 개발

---
+++ src/controllers/goal-controller.js
@@ -0,0 +1,19 @@
+const uploadPhotoAndValidate = require('../services/goal-service');
+
+// exports.imageTesting = async (req, res) => {
+//   console.log(req.file);
+//   res
+//     .status(200)
+//     .json({ message: 'Image uploaded successfully', file: req.file });
+// };
+
+exports.validatePhoto = async (req, res) => {
+  try {
+    const result = await uploadPhotoAndValidate(req);
+    res.status(200).json({ message: '인증 성공', data: result });
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: '에러로 인한 인증 실패', error: error.message });
+  }
+};
