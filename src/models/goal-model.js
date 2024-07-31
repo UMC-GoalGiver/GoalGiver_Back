afde1d1aa43727d0b051486d81b9e8b8b9cb8892
swoo0514 <swoo0514lim@gmail.com>
2024-07-31 18:44:40 +0900

    응답 데이터 수정

--- src/models/goal-model.js
+++ src/models/goal-model.js
@@ -2,8 +2,13 @@ const pool = require('../../config/database');
 
 exports.getGoalsByDateRange = async (week_start, week_end) => {
   const query =
-    'select g.id, title, description, start_date, end_date, type, status, latitude, longitude,  validation_type, emoji, donation_organization_id, donation_amount, gi.date from goals g, goalinstances gi where g.id = gi.goal_id and start_date >= ? and end_date <= ?';
+    'select g.id as goal_id, gi.id as goal_instance_id, title, description, start_date, end_date, type, status, latitude, longitude,  validation_type, emoji, donation_organization_id, donation_amount, gi.date from goals g, goalinstances gi where g.id = gi.goal_id and start_date >= ? and end_date <= ?';
 
-  const [rows] = await pool.execute(query, [week_start, week_end]);
-  return rows;
+  try {
+    const [rows] = await pool.execute(query, [week_start, week_end]);
+    return rows;
+  } catch (err) {
+    console.error('날짜 범위에 따른 에러: ', err);
+    throw err;
+  }
 };
