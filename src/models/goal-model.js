df193dd7d31aa0d6f2f170402da503e9867f0abf
swoo0514 <swoo0514lim@gmail.com>
2024-08-01 13:53:12 +0900

    상세 기능 구현 0801

--- src/models/goal-model.js
+++ src/models/goal-model.js
@@ -1,30 +1,29 @@
 const pool = require('../../config/database');
 
 exports.getGoalById = async (goalId) => {
-  const [rows] = pool.query('select * from goals where id = ?', [goalId]);
+  const [rows] = await pool.query('select * from goals where id = ?', [goalId]);
   return rows[0];
 };
 
-exports.saveValidationResult = async (goalId, userId, photoUrl) => {
+exports.saveValidationResult = async (goalId, photoUrl) => {
   await pool.query(
-    'insert into goal_validations (goal_id, user_id, validation_data values(?, ?, ?)',
-    [goalId, userId, photoUrl]
+    'insert into goal_validations (goal_id, validation_data) values(?, ?)',
+    [goalId, photoUrl]
   );
 };
 
-exports.notifyTeamMembers = async (goalId, userId, photoUrl) => {
+exports.notifyTeamMembers = async (goalId, user, photoUrl) => {
   const [members] = await pool.query(
     'select user_id from team_members where team_goal_id = ?',
     [goalId]
   );
   const goal = await this.getGoalById(goalId);
-  const requester = await pool.query('select name from user where id = ?', [
-    userId,
-  ]);
+  const requesterName = user.nickname; // res.locals.user에서 가져온 사용자 정보
+
   for (const member of members) {
-    if (member.user_id !== userId) {
+    if (member.user_id !== user.id) {
       const content = JSON.stringify({
-        message: `${requester}님께서 '${goal.title}' 인증을 요청을 보냈습니다.`,
+        message: `${requesterName}님께서 '${goal.title}' 인증을 요청을 보냈습니다.`,
         photoUrl: photoUrl,
         goal: {
           id: goal.id,
--- src/services/goal-service.js
+++ src/services/goal-service.js
@@ -4,18 +4,20 @@ const {
   notifyTeamMemebers,
 } = require('../models/goal-model');
 
-exports.uploadPhotoAndValidate = async (req) => {
+exports.uploadPhotoAndValidate = async (req, user) => {
   const photoUrl = req.file.location;
   const goalId = req.params.goalId;
-  const userId = req.user.id;
 
   const goal = await getGoalById(goalId);
-
+  if (user.id !== goal.user_id) {
+    // res.locals.user에서 가져온 사용자 ID
+    throw new Error('접근 권한이 없습니다. (아이디 불일치)');
+  }
   if (goal.type === 'personal') {
-    await saveValidationResult(goalId, userId, photoUrl);
+    await saveValidationResult(goalId, photoUrl);
     return photoUrl;
   } else if (goal.type === 'team') {
-    await notifyTeamMemebers(goalId, userId, photoUrl);
+    await notifyTeamMemebers(goalId, user, photoUrl);
     return photoUrl;
   } else {
     throw new Error('유효한 목표 타입이 아닙니다.');
