e04bfd2fec54a58932a722fcf3049f3653057d0a
swoo0514 <swoo0514lim@gmail.com>
2024-08-07 23:39:17 +0900

    테이블명 변경 0807

--- src/models/goal-model.js
+++ src/models/goal-model.js
@@ -2,7 +2,7 @@ const pool = require('../../config/database');
 
 exports.getGoalsByDateRange = async (week_start, week_end) => {
   const query =
-    'select g.id as goal_id, gi.id as goal_instance_id, title, description, start_date, end_date, type, status, latitude, longitude,  validation_type, emoji, donation_organization_id, donation_amount, gi.date from goals g, goalinstances gi where g.id = gi.goal_id and start_date >= ? and end_date <= ?';
+    'select g.id as goal_id, gi.id as goal_instance_id, title, description, start_date, end_date, type, status, latitude, longitude,  validation_type, emoji, donation_organization_id, donation_amount, gi.date from goals g, goal_instances gi where g.id = gi.goal_id and start_date >= ? and end_date <= ?';
 
   try {
     const [rows] = await pool.execute(query, [week_start, week_end]);
