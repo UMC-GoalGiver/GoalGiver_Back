8d691392563d5c965719307d011b7377b0df83de
방준혁 <qkd3001@naver.com>
2024-08-09 19:53:29 +0900

    상세 조회 팀 목표 TimeAttack

--- src/routes/validation-location-team-routes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const validationController = require('../controllers/validation-location-team-controller');
-
-router.get('/:goal_id/date-validation-history', validationController.getValidationLocationTeam);
-
-module.exports = router;
