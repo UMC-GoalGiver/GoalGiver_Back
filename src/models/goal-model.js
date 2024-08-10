bc43a00c15bad01a04a35275cac0a7ef60cd408b
swoo0514 <swoo0514lim@gmail.com>
2024-08-10 17:12:28 +0900

    사용자 id 검증 로직 추가

--- src/models/goal-model.js
+++ src/models/goal-model.js
@@ -8,12 +8,17 @@ const pool = require('../../config/database');
  * @returns {Promise<Array>} 목표 인스턴스 배열
  * @throws {Error} 데이터베이스 조회 에러
  */
-exports.getGoalsByDateRange = async (week_start, week_end) => {
-  const query =
-    'select g.id as goal_id, gi.id as goal_instance_id, title, description, start_date, end_date, type, status, latitude, longitude,  validation_type, emoji, donation_organization_id, donation_amount, gi.date from goals g, goal_instances gi where g.id = gi.goal_id and start_date >= ? and end_date <= ?';
+exports.getGoalsByDateRange = async (userId, week_start, week_end) => {
+  const query = `
+    SELECT g.id as goal_id, gi.id as goal_instance_id, title, description, start_date, end_date, type, status,
+    latitude, longitude, validation_type, emoji, donation_organization_id, donation_amount, gi.date
+    FROM goals g
+    JOIN goal_instances gi ON g.id = gi.goal_id
+    WHERE g.user_id = ? AND gi.date >= ? AND gi.date <= ?
+  `;
 
   try {
-    const [rows] = await pool.execute(query, [week_start, week_end]);
+    const [rows] = await pool.execute(query, [userId, week_start, week_end]);
     return rows;
   } catch (err) {
     console.error('날짜 범위에 따른 에러: ', err);
