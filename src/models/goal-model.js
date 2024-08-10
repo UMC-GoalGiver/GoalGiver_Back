765589ba50c066a090210d9ed57bc523e91d8160
swoo0514 <swoo0514lim@gmail.com>
2024-08-11 01:18:14 +0900

    인스턴스 관련 코드 수정 0811

--- src/models/goal-model.js
+++ src/models/goal-model.js
@@ -1,16 +1,42 @@
 const pool = require('../../config/database');
 
-exports.getGoalById = async (goalId) => {
-  const [rows] = await pool.query('select * from goals where id = ?', [goalId]);
+/**
+ * @function getGoalByInstanceId
+ * @description 인스턴스 ID로 목표 정보를 조회합니다.
+ * @param {number} instanceId - 조회할 목표 인스턴스 ID
+ * @returns {Promise<Object>} 목표 정보 객체
+ * @throws {Error} 데이터베이스 조회 에러
+ */
+exports.getGoalByInstanceId = async (instanceId) => {
+  const [rows] = await pool.query(
+    'SELECT g.*, gi.id as instance_id FROM goals g JOIN goal_instances gi ON g.id = gi.goal_id WHERE gi.id = ?',
+    [instanceId]
+  );
+
   if (rows.length === 0) {
-    throw new Error('Goal not found');
+    throw new Error('해당 목표를 찾을 수 없습니다.');
   }
+  console.log(rows[0]);
   return rows[0];
 };
 
-exports.insertGoalValidation = async (goalId, latitude, longitude) => {
+/**
+ * @function insertGoalValidation
+ * @description 새로운 목표 인증 레코드를 삽입합니다.
+ * @param {number} goalId - 목표 ID
+ * @param {number} instanceId - 목표 인스턴스 ID
+ * @param {number} latitude - 인증된 위도
+ * @param {number} longitude - 인증된 경도
+ * @returns {Promise<void>}
+ */
+exports.insertGoalValidation = async (
+  goalId,
+  instance_id,
+  latitude,
+  longitude
+) => {
   await pool.query(
-    'insert into goal_validation (goal_id, validation_data) values (?, ?)',
-    [goalId, JSON.stringify({ latitude, longitude })]
+    'insert into goal_validation (goal_id, goal_instance_id, validation_data) values (?, ?, ?)',
+    [goalId, instance_id, JSON.stringify({ latitude, longitude })]
   );
 };
--- src/services/goal-service.js
+++ src/services/goal-service.js
@@ -1,11 +1,23 @@
-const { getGoalById, insertGoalValidation } = require('../models/goal-model');
+const {
+  getGoalByInstanceId,
+  insertGoalValidation,
+} = require('../models/goal-model');
 
+/**
+ * @function calculateLocation
+ * @description 현재 위치를 저장된 목표 위치와 비교하여 인증합니다.
+ * @param {number} instanceId - 목표 인스턴스 ID
+ * @param {number} currentLatitude - 현재 위도
+ * @param {number} currentLongitude - 현재 경도
+ * @returns {Promise<boolean>} 위치가 허용 범위 내에 있는 경우 true, 그렇지 않으면 false를 반환
+ * @throws {Error} 목표를 찾을 수 없는 경우 오류 발생
+ */
 exports.calculateLocation = async (
-  goalId,
+  instanceId,
   currentLatitude,
   currentLongitude
 ) => {
-  const goal = await getGoalById(goalId);
+  const goal = await getGoalByInstanceId(instanceId);
   if (!goal) {
     throw new Error('목표가 없습니다');
   }
@@ -22,12 +34,26 @@ exports.calculateLocation = async (
 
   const allowedDistance = 50; // 오차 범위 - 50미터
   if (distance <= allowedDistance) {
-    await insertGoalValidation(goalId, currentLatitude, currentLongitude);
+    await insertGoalValidation(
+      goal.id,
+      instanceId,
+      currentLatitude,
+      currentLongitude
+    );
     return true;
   }
   return false;
 };
 
+/**
+ * @function calculateDistance
+ * @description 두 지점 간의 거리를 계산합니다.
+ * @param {number} lat1 - 첫 번째 지점의 위도
+ * @param {number} lon1 - 첫 번째 지점의 경도
+ * @param {number} lat2 - 두 번째 지점의 위도
+ * @param {number} lon2 - 두 번째 지점의 경도
+ * @returns {number} 두 지점 간의 거리 (미터)
+ */
 const calculateDistance = (lat1, lon1, lat2, lon2) => {
   const toRadians = (degrees) => degrees * (Math.PI / 180);
 
