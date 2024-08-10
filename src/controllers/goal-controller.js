83c9b5720ed2af4c1d2d06bc12d6fb9a68f1ab23
swoo0514 <swoo0514lim@gmail.com>
2024-08-10 15:38:56 +0900

    주석 추가 0810

--- src/controllers/goal-controller.js
+++ src/controllers/goal-controller.js
@@ -1,5 +1,12 @@
 const { getGoals } = require('../services/goal-service');
 
+/**
+ * @function getWeeklyGoals
+ * @description 주간 목표를 조회하여 클라이언트에 반환합니다.
+ * @param {Object} req - Express 요청 객체
+ * @param {Object} res - Express 응답 객체
+ * @param {Function} next - Express next 미들웨어 함수
+ */
 exports.getWeeklyGoals = async (req, res, next) => {
   const { week_start, week_end } = req.query;
 
@@ -11,9 +18,9 @@ exports.getWeeklyGoals = async (req, res, next) => {
   try {
     const goals = await getGoals(week_start, week_end);
 
-    res.json(goals);
+    res.status(200).json(goals);
   } catch (err) {
-    console.error(err);
+    console.error('주간 목표 조회 에러', err);
     next(err);
   }
 };
--- src/services/goal-service.js
+++ src/services/goal-service.js
@@ -1,6 +1,13 @@
 // ./src/services/goal-service.js
 const { getGoalsByDateRange } = require('../models/goal-model');
 
+/**
+ * @function getGoals
+ * @description 주어진 날짜 범위 내의 목표를 조회하여 구조화된 데이터로 반환합니다.
+ * @param {string} week_start - 조회 시작 날짜 (YYYY-MM-DD)
+ * @param {string} week_end - 조회 종료 날짜 (YYYY-MM-DD)
+ * @returns {Object} 주간 목표 데이터
+ */
 exports.getGoals = async (week_start, week_end) => {
   const goals = await getGoalsByDateRange(week_start, week_end);
 
