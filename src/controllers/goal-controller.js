7c0bdf1670994992583c7260eb4b0d5ce2db455f
swoo0514 <swoo0514lim@gmail.com>
2024-08-11 11:41:12 +0900

    상태코드 사용

--- src/controllers/goal-controller.js
+++ src/controllers/goal-controller.js
@@ -1,4 +1,5 @@
 const { getGoals } = require('../services/goal-service');
+const { StatusCodes } = require('http-status-codes');
 
 /**
  * @function getWeeklyGoals
@@ -10,18 +11,21 @@ const { getGoals } = require('../services/goal-service');
 exports.getWeeklyGoals = async (req, res, next) => {
   const { week_start, week_end } = req.query;
   const userId = res.locals.user.id;
+
   if (!week_start || !week_end) {
     return res
-      .status(400)
+      .status(StatusCodes.BAD_REQUEST)
       .json({ error: 'week_start 또는 week_end가 없습니다.' });
   }
+
   try {
     const goals = await getGoals(userId, week_start, week_end);
-
-    res.status(200).json(goals);
+    res.status(StatusCodes.OK).json(goals);
   } catch (err) {
     console.error('주간 목표 조회 에러', err);
-    res.status(500).json({ message: '주간 목표 조회 에러' });
+    res
+      .status(StatusCodes.INTERNAL_SERVER_ERROR)
+      .json({ message: '주간 목표 조회 에러' });
     next(err);
   }
 };
