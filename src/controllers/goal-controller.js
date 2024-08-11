bc4634bff2ecf5ee4c21864f13b49f627ed207f0
swoo0514 <swoo0514lim@gmail.com>
2024-08-11 14:44:30 +0900

    jest 전역변수 추가

--- src/controllers/goal-controller.js
+++ src/controllers/goal-controller.js
@@ -1,18 +1,20 @@
 const { uploadPhotoAndValidate } = require('../services/goal-service');
-
+const { StatusCodes } = require('http-status-codes');
 /**
  * @function validatePhoto
  * @description 목표 사진 인증을 처리하고 결과를 반환합니다.
  * @param {Object} req - Express 요청 객체
  * @param {Object} res - Express 응답 객체
  */
+
 exports.validatePhoto = async (req, res) => {
   try {
     const result = await uploadPhotoAndValidate(req, res.locals.user);
-    res.status(200).json({ message: '인증 성공', data: result });
+    res.status(StatusCodes.OK).json({ message: '인증 성공', data: result });
   } catch (error) {
-    res
-      .status(500)
-      .json({ message: '에러로 인한 인증 실패', error: error.message });
+    res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
+      message: '에러로 인한 인증 실패',
+      error: error.message,
+    });
   }
 };
