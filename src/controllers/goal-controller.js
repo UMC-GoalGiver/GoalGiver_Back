8fb37e150a62f782067fff8b2119e00eaceb8926
swoo0514 <swoo0514lim@gmail.com>
2024-08-11 15:09:49 +0900

    오류 세분화 및 테스트 코드 작성

--- src/controllers/goal-controller.js
+++ src/controllers/goal-controller.js
@@ -7,14 +7,29 @@ const { StatusCodes } = require('http-status-codes');
  * @param {Object} res - Express 응답 객체
  */
 
+// src/controllers/goal-controller.js
+
 exports.validatePhoto = async (req, res) => {
   try {
     const result = await uploadPhotoAndValidate(req, res.locals.user);
     res.status(StatusCodes.OK).json({ message: '인증 성공', data: result });
   } catch (error) {
-    res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
-      message: '에러로 인한 인증 실패',
-      error: error.message,
-    });
+    if (error.message.includes('접근 권한이 없습니다')) {
+      // 권한 오류
+      res.status(StatusCodes.FORBIDDEN).json({
+        message: '접근 권한이 없습니다. (아이디 불일치)',
+      });
+    } else if (error.message.includes('유효한 목표 타입이 아닙니다')) {
+      // 잘못된 요청 오류
+      res.status(StatusCodes.BAD_REQUEST).json({
+        message: '유효한 목표 타입이 아닙니다.',
+      });
+    } else {
+      // 기타 서버 오류
+      res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
+        message: '에러로 인한 인증 실패',
+        error: error.message,
+      });
+    }
   }
 };
