56f19f5dd74fde44267c3556ff9d508048c8f8eb
MinJae Han <hanminjae131@gmail.com>
2024-08-23 07:46:27 +0900

    테스트 및 index정리

--- src/index.js
+++ src/index.js
@@ -3,19 +3,14 @@ const dotenv = require('dotenv');
 const helmet = require('helmet');
 const morgan = require('morgan');
 const tokenRouter = require('./routes/token-route');
-
 const goalRouter = require('./routes/goal-route');
 const authRoutes = require('../src/routes/auth-routes.js');
 const friendRoutes = require('../src/routes/friend-routes.js');
-
-const { setTestUser } = require('./middlewares/set-test-user');
-
 const notificationRouter = require('./routes/notification-route');
-
 dotenv.config();
 const mypageRouter = require('./routes/mypage-route'); // 작성자: Minjae Han
 
-//준구리 부분
+// 준구리 부분
 const weeklyGoalRoutes = require('./routes/weekly-goal-routes');
 const monthlyGoalRoutes = require('./routes/monthly-goal-routes.js');
 const yearlyGoalRoutes = require('./routes/yearly-goal-routes.js');
@@ -44,12 +39,10 @@ if (process.env.NODE_ENV === 'production') {
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.use(setTestUser);
-
-//로그인 라우터
+// 로그인 라우터
 app.use('/api/auth', authRoutes);
 
-app.use('/api/friends', friendRoutes); //친구목록 라우터
+app.use('/api/friends', friendRoutes); // 친구목록 라우터
 
 app.use('/goals', goalRouter);
 
@@ -59,18 +52,14 @@ app.use('/notification', notificationRouter);
 
 app.use('/token', tokenRouter);
 
-//준구리 부분
-app.use('/goals/week', weeklyGoalRoutes); //주간 진행상황 라우터
-app.use('/goals/month', monthlyGoalRoutes); //월간 진행상황 라우터
-app.use('/goals/year', yearlyGoalRoutes); //연간 진행상황 라우터
-app.use('/goals/location-team/list', validationLocationAndTeamRoutes); //위치인증, 팀원인증 인증내역 라우터
-app.use('/goals/timeattack/progress', teamGoalTimeAttackRoutes); //팀목표 진행상황 라우터
-app.use('/goals/timeattack/list', teamGoalTimeAttackValidationRoutes); //팀목표 인증내역 라우터
-app.use('/goals/photo/list', validationPhotoRoutes); //사진 인증내역 라우터
-
-app.use('/', (req, res) => {
-  res.send('아무것도 없슴');
-});
+// 준구리 부분
+app.use('/goals/week', weeklyGoalRoutes); // 주간 진행상황 라우터
+app.use('/goals/month', monthlyGoalRoutes); // 월간 진행상황 라우터
+app.use('/goals/year', yearlyGoalRoutes); // 연간 진행상황 라우터
+app.use('/goals/location-team/list', validationLocationAndTeamRoutes); // 위치인증, 팀원인증 인증내역 라우터
+app.use('/goals/timeattack/progress', teamGoalTimeAttackRoutes); // 팀목표 진행상황 라우터
+app.use('/goals/timeattack/list', teamGoalTimeAttackValidationRoutes); // 팀목표 인증내역 라우터
+app.use('/goals/photo/list', validationPhotoRoutes); // 사진 인증내역 라우터
 
 app.use((req, res, next) => {
   const error = new Error(`${req.method} ${req.url} 라우터가 없습니다.`);
