86e45ccfa9a9b64bf3985d04c79d5cdfb2863591
Lims <swoo0514lim@gmail.com>
2024-08-24 11:07:08 +0900

    알림 조회 기능 추가

--- src/controllers/notification-controller.js
+++ src/controllers/notification-controller.js
@@ -1,6 +1,9 @@
 //notification-controller.js
 
-const { sendNotification } = require('../services/notification-service');
+const {
+  sendNotification,
+  getNotificationsByUserId,
+} = require('../services/notification-service');
 const { StatusCodes } = require('http-status-codes');
 /**
  * @function sendNotificationController
@@ -21,5 +24,21 @@ exports.sendNotificationController = async (req, res) => {
     res
       .status(StatusCodes.INTERNAL_SERVER_ERROR)
       .json({ message: '알림 전송 실패', error: err.message });
+  }
+};
+
+exports.getNotifications = async (req, res) => {
+  try {
+    const userId = req.user.id;
+
+    const response = await getNotificationsByUserId(userId);
+
+    res
+      .status(StatusCodes.OK)
+      .json({ message: '알림 조회 성공', data: response });
+  } catch (err) {
+    res
+      .status(StatusCodes.INTERNAL_SERVER_ERROR)
+      .json({ message: '알림 도회 실패', error: err.message });
   }
 };
